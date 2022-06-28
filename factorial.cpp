54bec7ca1b885a3e6f77ca03754d43e357c6e02c
DiegoAlcoba <diegoalcobaarias@gmail.com>
2022-06-28 20:27:11 +0200

    Hola

---
+++ factorial.cpp
@@ -0,0 +1,16 @@
+#include "factorial.h"
+
+Factorial::Factorial()
+{
+
+}
+
+int Factorial::calculaFactorial() {
+
+    int f = 47284564;
+
+    for (int i = 1; i <= f; i++) {
+
+        f = f * i;
+    }
+}
---
+++ factorial.h
@@ -0,0 +1,12 @@
+#ifndef FACTORIAL_H
+#define FACTORIAL_H
+
+
+class Factorial
+{
+public:
+    Factorial();
+    int calculaFactorial()
+};
+
+#endif // FACTORIAL_H
